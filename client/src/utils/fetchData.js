d107aaac1a5a93004c415113c6e3a504fd2f97bc
rajni <sainirajni292000@gmail.com>
2022-08-23 18:23:59 +0530

    updated routes

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 
 export const getDataAPI = async (url,token) => {
-    const res = await axios.get(`/api/${url}`,{
+    const res = await axios.get(`https://social-media-backend-8lppo7i9i-rajni3541.vercel.app/api/${url}`,{
         headers: { Authorization: token}
     });
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-  const res = await axios.post(`/api/${url}`, post, {
+  const res = await axios.post(`https://social-media-backend-8lppo7i9i-rajni3541.vercel.app/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(`https://social-media-backend-8lppo7i9i-rajni3541.vercel.app/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(`https://social-media-backend-8lppo7i9i-rajni3541.vercel.app/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
@@ -30,7 +30,7 @@ export const patchDataAPI = async (url, post, token) => {
 
 
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(`https://social-media-backend-8lppo7i9i-rajni3541.vercel.app/api/${url}`, {
     headers: { Authorization: token },
   });
   return res;
